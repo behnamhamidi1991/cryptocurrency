a6fc3d11cbe32ef4cb2851a7d00ccf84d3ace0a6
Behnam Hamidi <eslp91@hotmail.com>
2024-04-17 09:42:47 +0330

    upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>صفحه اصلی</div>;
+  return (
+    <div>
+      <h1>صفحه اصلی</h1>
+      <p>این یک متن تستی جهت آزمایش فنت می باشد</p>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>HEADER SAMPLE</div>;
+};
+
+export default Header;
