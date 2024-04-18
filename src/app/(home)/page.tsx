bc105f635a4aa916ee0457a2b367b65b41355a38
Behnam Hamidi <eslp91@hotmail.com>
2024-04-18 11:37:01 +0330

    data

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Slider from "@/components/Home/Main/Slider/Slider";
 import Icons from "@/components/Home/Main/Icons/Icons";
 import Market from "@/components/Home/Main/Market/Market";
+import Blog from "@/components/Home/Main/Blog/Blog";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <Slider />
       <Icons />
       <Market />
+      <Blog />
     </div>
   );
 };
--- src/components/Home/Main/Blog/Blog.tsx
+++ src/components/Home/Main/Blog/Blog.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import "./blog.scss";
+import { blogPosts } from "@/data";
+import Image from "next/image";
 
 const Blog = () => {
-  return <div></div>;
+  return (
+    <div className="blog">
+      <div className="blogHeader">
+        <h2>آخرین خبرها</h2>
+        <p>
+          مطالب این بخش از سایر خبرگزاری ها جمع آوری شده است و صرفاً جهت افزایش
+          آگاهی شما قرار گرفته است و لزوماً به معنای تایید آن توسط اکسچنج کوین
+          نیست.
+        </p>
+      </div>
+      <div className="post-wrapper">
+        {blogPosts.map((item) => (
+          <div key={item.id} className="post-box">
+            <div className="post-image-container">
+              <Image src={item.image} alt={item.title} className="post-image" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
