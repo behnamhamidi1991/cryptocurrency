dedc08d6852ef785490210a405a2dafdd381bbd9
Behnam Hamidi <eslp91@hotmail.com>
2024-04-18 07:05:57 +0330

    header start

--- src/components/Home/Header/Header.tsx
+++ src/components/Home/Header/Header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import "./header.scss";
 import { IoMdHome } from "react-icons/io";
 import { FaChartSimple } from "react-icons/fa6";
 import { CgArrowsExchangeAlt } from "react-icons/cg";
@@ -12,9 +13,36 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-right">
-        <ul className="header-nav-links">
-          <li>
-            <Link href="/">لینک آزمایشی</Link>
+        <ul className="nav-links">
+          <li className="nav-item">
+            <Link href="/" className="nav-link">
+              <IoMdHome className="nav-link-icon" />
+              <p>خانه</p>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/" className="nav-link">
+              <FaChartSimple className="nav-link-icon" />
+              <p>قیمت لحظه ای</p>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/" className="nav-link">
+              <CgArrowsExchangeAlt className="nav-link-icon" />
+              <p>بازار</p>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/" className="nav-link">
+              <RiCoinsFill className="nav-link-icon" />
+              <p>تبدیلگر</p>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/" className="nav-link">
+              <FaWallet className="nav-link-icon" />
+              <p>کیف پول</p>
+            </Link>
           </li>
         </ul>
       </div>
