56bf2f41b9fd033445cbb561d26d585b8113eabe
Behnam Hamidi <eslp91@hotmail.com>
2024-04-18 08:53:08 +0330

    swiper

--- src/components/Home/Main/Slider/Slider.tsx
+++ src/components/Home/Main/Slider/Slider.tsx
@@ -1,34 +1,34 @@
 "use client";
-
 import React from "react";
 import "./slider.scss";
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import { Pagination, Autoplay } from "swiper/modules";
+import { featured } from "@/data";
+import Image from "next/image";
 
 const Slider = () => {
   return (
     <>
       <Swiper
-        slidesPerView={"auto"}
-        spaceBetween={30}
+        slidesPerView={4}
+        spaceBetween={10}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination]}
-        className="mySwiper"
+        modules={[Pagination, Autoplay]}
+        className="swiper-container"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {featured.map((item) => (
+          <SwiperSlide key={item.id} className="swiper-box">
+            <Image src={item.image} alt={item.title} className="slider-image" />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
--- src/data.ts
+++ src/data.ts
@@ -6,11 +6,53 @@ interface FeaturedPosts {
 
 import { StaticImageData } from "next/image";
 import image1 from "./assets/images/1.jpg";
+import image2 from "./assets/images/2.jpg";
+import image3 from "./assets/images/3.jpg";
+import image4 from "./assets/images/4.jpg";
+import image5 from "./assets/images/5.jpg";
+import image6 from "./assets/images/6.jpg";
+import image7 from "./assets/images/7.jpg";
+import image8 from "./assets/images/8.jpg";
 
 export const featured: FeaturedPosts[] = [
   {
     id: 1,
     title: "آموزش ترید در بازارهای مالی",
     image: image1,
+  },
+  {
+    id: 2,
+    title: "چگونه خود را برای نوسانات بازار آماده کنیم؟",
+    image: image2,
+  },
+  {
+    id: 3,
+    title: "آموزش کار با صرافی",
+    image: image3,
+  },
+  {
+    id: 4,
+    title: "آموزش کار با صرافی",
+    image: image4,
+  },
+  {
+    id: 5,
+    title: "آموزش کار با صرافی",
+    image: image5,
+  },
+  {
+    id: 6,
+    title: "آموزش کار با صرافی",
+    image: image6,
+  },
+  {
+    id: 7,
+    title: "آموزش کار با صرافی",
+    image: image7,
+  },
+  {
+    id: 8,
+    title: "آموزش کار با صرافی",
+    image: image8,
   },
 ];
