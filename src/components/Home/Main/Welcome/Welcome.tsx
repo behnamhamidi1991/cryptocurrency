fd2809a2a06eed1f6ad1eb61f7c26416507a97af
Behnam Hamidi <eslp91@hotmail.com>
2024-04-18 07:29:59 +0330

    Fix

--- src/components/Home/Main/Welcome/Welcome.tsx
+++ src/components/Home/Main/Welcome/Welcome.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "./welcome.scss";
+import { TbLogin } from "react-icons/tb";
+import { MdCardGiftcard } from "react-icons/md";
 
 const Welcome = () => {
   return (
-    <div>
-      <p>پیج خوش آمد گویی</p>
-      <p>این پیج برای خوش آمد گویی و ثبت نام می باشد</p>
+    <div className="main-welcome">
+      <div className="welcome-top">
+        <h2>به اکسچنج کوین خوش آمدید</h2>
+        <p>برای استفاده از خدمات اپلیکیشن وارد حساب کاربری خود شوید</p>
+      </div>
+      <div className="main-welcome-btnContainer">
+        <button className="login">
+          <TbLogin />
+          <span>ورود</span>
+        </button>
+        <button className="register"></button>
+      </div>
     </div>
   );
 };
