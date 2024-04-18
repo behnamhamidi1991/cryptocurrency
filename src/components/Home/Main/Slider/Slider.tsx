dc3a167d79e716f8c457ef15088b0958f1334505
Behnam Hamidi <eslp91@hotmail.com>
2024-04-18 08:59:15 +0330

    slider

--- src/components/Home/Main/Slider/Slider.tsx
+++ src/components/Home/Main/Slider/Slider.tsx
@@ -21,6 +21,24 @@ const Slider = () => {
         pagination={{
           clickable: true,
         }}
+        breakpoints={{
+          350: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          650: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          980: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+          1369: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+        }}
         modules={[Pagination, Autoplay]}
         className="swiper-container"
       >
